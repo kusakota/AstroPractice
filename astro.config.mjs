9100186475cd7735f5a61623944d54e5f2b2954b
kusakota <kusakota2015@gmail.com>
2024-08-11 00:53:26 +0900

    scssの導入と各種stylesの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    css: ["@/styles/style.scss","@/styles/reset.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                // importしたいファイルを記載 
+                additionalData: `@import "src/styles/_mixin.scss";`
+            }
+          }
+        }
+    }
+});
