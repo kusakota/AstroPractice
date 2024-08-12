067c0b6f67865bb2f0de623534dc9529a0113c45
kusakota <kusakota2015@gmail.com>
2024-08-13 01:35:48 +0900

    ハンバーガーメニューとreset.scssのimportを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-    css: ["@/styles/style.scss","@/styles/reset.scss"],
     vite: {
         css: {
             preprocessorOptions: {
                 scss: {
                 // importしたいファイルを記載 
-                additionalData: `@import "src/styles/_mixin.scss";`
+                additionalData: `@import "src/styles/_mixin.scss","src/styles/_variables";`
             }
           }
         }
