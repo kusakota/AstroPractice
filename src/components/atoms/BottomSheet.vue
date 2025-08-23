c86f11ed50de760947488862451af35200985ddf
kusakota <kusakota2015@gmail.com>
2025-08-23 22:43:06 +0900

    ts化とレスポンシブ対応

--- src/components/atoms/BottomSheet.vue
+++ src/components/atoms/BottomSheet.vue
@@ -1,15 +1,21 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue';
 
 // シートデフォルトの垂直位置
 const scrollY = ref(300);
+let maxScrollY:number; //ボトムシートの下限
+
+onMounted(() =>{
+  let vh = window.innerHeight
+  maxScrollY = vh - 120
+})
+
 // ドラッグ中の状態
 let startY = 0;
 let baseY = scrollY.value;
 let isDragging = false;
 
-const maxScrollY = ref(1100);
-const minScrollY = ref(10);
+const minScrollY = 0;
 
 /**
  * onTouchStart: トッチドラッグ開始ハンドラ
@@ -19,7 +25,7 @@ const minScrollY = ref(10);
  * - touchmove と touchend リスナを登録
  * @param {TouchEvent} event - タッチ開始イベント
  */
-function onTouchStart(event) {
+function onTouchStart(event:TouchEvent) {
   isDragging = true;
   startY = event.touches[0].clientY;
   baseY = scrollY.value;
@@ -35,11 +41,11 @@ function onTouchStart(event) {
  * - 移動量（event.touches[0].clientY - startY）を計算し scrollY を更新
  * @param {TouchEvent} event - タッチ移動イベント
  */
-function onTouchMove(event) {
+function onTouchMove(event:TouchEvent) {
   if (!isDragging) return;
   const newY = baseY + (event.touches[0].clientY - startY);
   // ドラッグ中に下限を下回らないようクランプ
-  scrollY.value = Math.max(minScrollY.value, newY);
+  scrollY.value = Math.max(minScrollY, newY);
 }
 
 /**
@@ -50,7 +56,7 @@ function onTouchMove(event) {
 function onTouchEnd() {
   isDragging = false;
   // ドラッグ終了時にスクロール位置を範囲内にクランプ
-  scrollY.value = Math.min(maxScrollY.value, Math.max(minScrollY.value, scrollY.value));
+  scrollY.value = Math.min(maxScrollY, Math.max(minScrollY, scrollY.value));
   document.removeEventListener('touchmove', onTouchMove);
   document.removeEventListener('touchend', onTouchEnd);
   // ページスクロールを解除
@@ -64,7 +70,7 @@ function onTouchEnd() {
  * - mousemove と mouseup リスナを登録
  * @param {MouseEvent} event - マウスダウンイベント
  */
-function onMouseDown(event) {
+function onMouseDown(event:MouseEvent) {
   isDragging = true;
   startY = event.clientY;
   baseY = scrollY.value;
@@ -80,11 +86,11 @@ function onMouseDown(event) {
  * - 移動量（event.clientY - startY）を計算し scrollY を更新
  * @param {MouseEvent} event - マウス移動イベント
  */
-function onMouseMove(event) {
+function onMouseMove(event:MouseEvent) {
   if (!isDragging) return;
   const newY = baseY + (event.clientY - startY);
   // ドラッグ中に下限を下回らないようクランプ
-  scrollY.value = Math.max(minScrollY.value, newY);
+  scrollY.value = Math.max(minScrollY, newY);
 }
 
 /**
@@ -95,7 +101,7 @@ function onMouseMove(event) {
 function onMouseUp() {
   isDragging = false;
   // ドラッグ終了時にスクロール位置を範囲内にクランプ
-  scrollY.value = Math.min(maxScrollY.value, Math.max(minScrollY.value, scrollY.value));
+  scrollY.value = Math.min(maxScrollY, Math.max(minScrollY, scrollY.value));
   document.removeEventListener('mousemove', onMouseMove);
   document.removeEventListener('mouseup', onMouseUp);
   // ページスクロールを解除
@@ -109,7 +115,7 @@ function onMouseUp() {
  * - pointermove と pointerup リスナを登録
  * @param {PointerEvent} event - ポインタ開始イベント
  */
-function onPointerDown(event) {
+function onPointerDown(event: PointerEvent) {
   event.preventDefault();
   isDragging = true;
   startY = event.clientY;
@@ -126,11 +132,11 @@ function onPointerDown(event) {
  * - 移動量（event.clientY - startY）を計算し scrollY を更新
  * @param {PointerEvent} event - ポインタ移動イベント
  */
-function onPointerMove(event) {
+function onPointerMove(event: PointerEvent) {
   if (!isDragging) return;
   const newY = baseY + (event.clientY - startY);
   // ドラッグ中に下限を下回らないようクランプ
-  scrollY.value = Math.max(minScrollY.value, newY);
+  scrollY.value = Math.max(minScrollY, newY);
 }
 
 /**
@@ -141,7 +147,7 @@ function onPointerMove(event) {
 function onPointerUp() {
   isDragging = false;
   // ドラッグ終了時にスクロール位置を範囲内にクランプ
-  scrollY.value = Math.min(maxScrollY.value, Math.max(minScrollY.value, scrollY.value));
+  scrollY.value = Math.min(maxScrollY, Math.max(minScrollY, scrollY.value));
   document.removeEventListener('pointermove', onPointerMove);
   document.removeEventListener('pointerup', onPointerUp);
   // ページスクロールを解除
@@ -150,7 +156,7 @@ function onPointerUp() {
 </script>
 
 <template>
-  <div class="bottom-sheet__wrapper shadow" :style="`transform: translate3d(0px, ${scrollY}px, 0px)`" @touchstart="onTouchStart" @mousedown="onMouseDown" @pointerdown="onPointerDown">
+  <div class="bottom-sheet__wrapper shadow" :style="`transform: translate(0px, ${scrollY}px)`" @touchstart="onTouchStart" @mousedown="onMouseDown" @pointerdown="onPointerDown">
       <div class="bottom-sheet__handle" >
         <div class="bottom-sheet__handle--bar" />
       </div>
@@ -163,7 +169,7 @@ function onPointerUp() {
 <style lang="scss" scoped>
 .bottom-sheet{
   &__wrapper {
-    height: calc(100vh - 60px);
+    height: calc(100dvh - 60px);
     position: fixed;
     bottom: 0;
     left: 0;
